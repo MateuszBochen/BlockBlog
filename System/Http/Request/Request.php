fdaea3458b53b569f44a2a993752cd0afea18238
Mateusz Bochen <bochen.mateusz@gmail.com>
2015-03-23 21:57:37 +0100

    Little progress

--- System/Http/Request/Request.php
+++ System/Http/Request/Request.php
@@ -6,22 +6,53 @@ class Request
 {
     private $applicatopnPath;
     private $urlParams;
-    
+    private $myPost = [];
+    private $myGet = [];
+
     public function __construct()
     {
         $this->applicatopnPath = '/'.implode(explode('/', $_SERVER['PHP_SELF'], -1));
         $this->urlParams = explode('/', trim(str_replace($this->applicatopnPath, '', $_SERVER['REDIRECT_URL']), '/'));
-        /* echo '<pre>';
-        var_dump($this->applicatopnPath);
-        print_r($_SERVER);
-        print_r($this->urlParams); */
+
+        $this->myPost = $_POST;
+        $this->myGet = $_GET;
     }
-    
+
     public function getUrlParam($index)
     {
         if (isset($this->urlParams[$index])) {
             return $this->urlParams[$index];
         }
         return null;
+    }
+
+    public function post($name)
+    {
+        return $this->getFromArray($name, $this->myPost);
+    }
+
+    public function get($name)
+    {
+        return $this->getFromArray($name, $this->myGet);
+    }
+
+    private function getFromArray($index, &$array)
+    {
+        $paramAsArray = explode('.', $index);
+        $lastValue = $array;
+
+        foreach ($paramAsArray as $index) {
+            if (!isset($lastValue[$index])) {
+                return '';
+            }
+
+            if (!is_array($lastValue[$index])) {
+                return $lastValue[$index];
+            }
+
+            $lastValue = $lastValue[$index];
+        }
+
+        return $lastValue;
     }
 }
