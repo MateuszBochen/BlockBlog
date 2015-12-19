4f7bf37d45f08b4f94477b090b6e9fd834b39a1f
Mateusz Bochen <bochen.mateusz@gmail.com>
2015-12-19 22:15:46 +0100

    Update save page method

--- MainApp/Pages/Create.php
+++ MainApp/Pages/Create.php
@@ -3,6 +3,7 @@
 namespace MainApp\Pages;
 
 use System\AppLauncher\Admin\ExtendsClass\BaseApp;
+use System\Http\Form\Validators;
 
 class Create extends BaseApp
 {
@@ -10,6 +11,43 @@ class Create extends BaseApp
 
     public function init()
     {
-        return $this->render->base('newPage', []);
+        $request = $this->get('request');
+        $page = new Entities\Page();
+
+        if ($request->isPost()) {
+            $this->save($page);
+        }
+
+        return $this->render->base('newPage', ['page' => $page]);
+    }
+
+
+    private function save($page)
+    {
+        $this->orm = $this->get('orm');
+        $notifications = $this->get('notifications');
+
+        $form = $this->get('form');
+        $form->setEntity($page);
+        $form->addRule('pageName', new Validators\NotEmpty());
+        $form->addRule('pageUrl', new Validators\NotEmpty());
+        $form->addRule('pageStructJson', new Validators\NotEmpty());
+
+        if($form->valid()) {
+            $this->orm->add($page);
+            $this->orm->save();
+
+            $notifications->add('Page was created', 'success');
+        }
+        else {
+            $errors = $form->getErrors();
+            $message = '';
+
+            foreach ($errors as $name => $messages) {
+                $message .= $messages['name'].': '.implode(', ', $messages['messages']).'<br />';
+            }
+
+            $notifications->add($message, 'error');
+        }
     }
 }
